92ed0df5aca626bce3d6641308a634b34c50819b
Min-Hyeok <alsgur0008@naver.com>
2022-08-14 16:15:28 +0900

    fontawesome 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faCamera } from '@fortawesome/free-solid-svg-icons';
 
 const Text = styled.p`
   color: ${({ theme }) => theme.primary};
@@ -6,6 +8,12 @@ const Text = styled.p`
 `;
 
 function Home() {
-  return <Text>안녕</Text>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faCamera} />
+      <FontAwesomeIcon icon={faMagnifyingGlass} />
+      <Text>안녕</Text>
+    </div>
+  );
 }
 export default Home;
