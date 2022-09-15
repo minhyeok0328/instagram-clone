3803786927c54ce3fdd36691db3bdf1f87228cad
todnks <tpdnr01@gmail.com>
2022-09-16 00:04:43 +0900

    스타일,애니메이션 추가

--- frontend/src/components/RecommendCard.tsx
+++ frontend/src/components/RecommendCard.tsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 import RecommendCardItem from '@components/RecommendCardItem';
+import useFakeFeedList from '@hooks/useFakeFeedList';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 const CardList = styled.div`
   display: flex;
   flex-grow: 1;
+  transition: 0.5s;
 `;
 const Recommend = styled.div`
+  position: relative;
   height: 300px;
   background-color: ${({ theme }) => theme.colors.white};
   margin: auto;
@@ -29,8 +35,57 @@ const CardHeader = styled.div`
     color: ${({ theme }) => theme.colors.button};
   }
 `;
-
-function RecommendCard() {
+const Prevbtn = styled.div`
+  > div {
+    background-color: ${({ theme }) => theme.colors.secondfont};
+    color: ${({ theme }) => theme.colors.white};
+    font-size: 15px;
+    border-radius: 50%;
+  }
+  text-align: center;
+  line-height: 24px;
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 50%;
+  z-index: 1;
+  margin: ${({ theme }) => theme.space.sm};
+  overflow: hidden;
+  white-space: nowrap;
+`;
+const nextstyle = {
+  right: '0',
+};
+interface List {
+  Count: Number;
+  setCount: Function;
+}
+function RecommendCard({ Count, setCount, ...props }: List) {
+  const [nextbtn, Setnext] = useState(true);
+  const [prevbtn, Setprev] = useState(false);
+  const fakeFeedList = useFakeFeedList();
+  const maxCount = fakeFeedList.length - 2;
+  function Pagemove(event: String) {
+    if (event === 'prev') {
+      setCount((Count: number) => Count - 1);
+      if (Count === 1) {
+        Setprev(false);
+      }
+      Setnext(true);
+    }
+    if (event === 'next') {
+      if (maxCount > Count) {
+        setCount((Count: number) => Count + 1);
+      }
+      if (maxCount - 1 === Count) {
+        Setnext(false);
+      }
+      Setprev(true);
+    }
+  }
+  const styled = {
+    transform: `translateX(calc(-200px * ${Count}))`,
+  };
   return (
     <Recommend>
       <CardHeader>
@@ -39,9 +94,25 @@ function RecommendCard() {
           <div>모두 보기</div>
         </div>
       </CardHeader>
-      <CardList>
-        <RecommendCardItem></RecommendCardItem>
+      {prevbtn && (
+        <Prevbtn onClick={() => Pagemove('prev')}>
+          <div>
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </div>
+        </Prevbtn>
+      )}
+      <CardList style={styled}>
+        {fakeFeedList.map((feedItem, key) => (
+          <RecommendCardItem {...feedItem} key={key}></RecommendCardItem>
+        ))}
       </CardList>
+      {nextbtn && (
+        <Prevbtn style={nextstyle} onClick={() => Pagemove('next')}>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </div>
+        </Prevbtn>
+      )}
     </Recommend>
   );
 }
