443f2086777491c2014f19b7eca410b539b1a836
todnks <tpdnr01@gmail.com>
2022-09-25 00:57:28 +0900

    (#1) 메인페이지 Feed map추가

--- frontend/src/components/BaseMain.tsx
+++ frontend/src/components/BaseMain.tsx
@@ -1,51 +1,71 @@
 import styled from 'styled-components';
 import RecommendCard from '@components/RecommendCard';
 import useListCount from '@hooks/useListCount';
+import useFakeFeedList from '@hooks/useFakeFeedList';
+import Feed from '@components/Feed';
+import Sidemenu from '@components/Sidemenu';
+import Follow from '@components/Follow';
 const Main = styled.main`
   background-color: ${({ theme }) => theme.colors.background};
   > div {
     display: flex;
     margin: auto;
     padding-top: ${({ theme }) => theme.space.md};
+    max-width: 820px;
   }
   align-items: stretch;
   margin: 0;
   padding: 0;
 `;
 const MaincContainer = styled.section`
-  width: 100%;
   margin: auto;
-  @media (min-width: 1000px) {
-    margin-right: ${({ theme }) => theme.space.md};
-  }
 `;
-// const MainTitle = styled.h4`
-//   font-size: 16px;
-//   font-weight: bold;
-// `;
 const Maincontent = styled.div`
   margin: auto;
+  margin-bottom: ${({ theme }) => theme.space.md};
   margin-top: ${({ theme }) => theme.space.sm};
+  border: 1px solid ${({ theme }) => theme.colors.line};
+  background-color: ${({ theme }) => theme.colors.white};
   border-radius: 8px;
   width: 470px;
-  margin: auto;
+  overflow: hidden;
 `;
+const Feddstyle = {
+  margin: '16px 0',
+};
 function BaseMain() {
   const ListCount = useListCount(0);
+  const fakeFeedList = useFakeFeedList();
+  const List = fakeFeedList.map((feedItem, key) => {
+    if (key === 2) {
+      return (
+        <>
+          <Maincontent>
+            <RecommendCard {...ListCount} key={key}></RecommendCard>
+          </Maincontent>
+          <Maincontent>
+            <Feed {...feedItem} key={key} />
+          </Maincontent>
+        </>
+      );
+    } else {
+      return (
+        <Maincontent>
+          <Feed {...feedItem} key={key} />
+        </Maincontent>
+      );
+    }
+  });
   return (
     <Main>
       <div>
         <MaincContainer>
-          {/* <Maincontent>
-            {fakeFeedList.map((feedItem, key) => (
-              <Feed {...feedItem} key={key} />
-            ))}
-          </Maincontent> */}
-          <Maincontent>
-            <RecommendCard {...ListCount}></RecommendCard>
+          <Maincontent style={Feddstyle}>
+            <Follow />
           </Maincontent>
+          {List}
         </MaincContainer>
-        {/* <Sidemenu /> */}
+        <Sidemenu />
       </div>
     </Main>
   );
