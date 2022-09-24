600df87d8e6f1f000378c64debac138b8fd5d97d
todnks <tpdnr01@gmail.com>
2022-09-25 00:51:04 +0900

    router재설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-    </Routes>
+    <App />
   </BrowserRouter>,
 );
 
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,10 +1,15 @@
 import BaseMain from '@components/BaseMain';
 import { Route, Routes } from 'react-router-dom';
+import Feedpopup from '@components/Feedpopup';
+import Recommend from '@components/Recommend';
+
 function Main() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<BaseMain />}></Route>
+        <Route path="" element={<BaseMain />}></Route>
+        <Route path=":id" element={<Feedpopup />}></Route>
+        <Route path="/explore" element={<Recommend />}></Route>
       </Routes>
     </>
   );
