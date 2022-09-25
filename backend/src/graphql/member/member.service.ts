d7c7867cb9225c714389f16e163fc3f8e0ca0791
mh.kim <mh.kim@dnd-lab.com>
2022-09-25 19:10:33 +0900

    test commit

--- backend/src/graphql/member/member.service.ts
+++ backend/src/graphql/member/member.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MemberService {
   getHelloWorld(): string {
-    return 'Hello World!';
+    return 'Hello World! test';
   }
 }
