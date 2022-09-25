13a94eff24fe47ab12ad39124429e548e1dc5dcd
mh.kim <mh.kim@dnd-lab.com>
2022-09-25 19:11:16 +0900

    slack notification test

--- backend/src/graphql/member/member.service.ts
+++ backend/src/graphql/member/member.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MemberService {
   getHelloWorld(): string {
-    return 'Hello World! test';
+    return 'Hello World!';
   }
 }
